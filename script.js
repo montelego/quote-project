c5c2825a233c3e245fd51fc27c2e557720a01cfa
Muhsini Mohamed <muhcnlego@gmail.com>
2024-12-08 19:19:17 +0200

    quote project first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector(".btn ");
+// const url =
+("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political,racist,sexist,explicit&type=single");
+
+// const jokeApi = "https://official-joke-api.appspot.com/jokes/random";
+
+const qAp =
+  "http://cors-anywhere.herokuapp.com/https://rapidapi.com/HealThruWords/api/universal-inspirational-quotes/playground/53cc559de4b06cf88bc65e0a";
+
+const sampApi =
+  "http://cors-anywhere.herokuapp.com/https://zenquotes.io/api/random";
+
+btn.addEventListener("click", joke);
+
+async function joke() {
+  const response = await fetch(sampApi);
+  const data = await response.json();
+
+  data.forEach((sampApi) => {
+    const quote = sampApi.q;
+    const author = sampApi.a;
+    document.querySelector(".content p").innerHTML = `"${quote}"`;
+    document.querySelector(".name").innerHTML = `Author:&nbsp;${author}`;
+    document.querySelector(".name").style.display = "block";
+  });
+
+  console.log(data);
+
+  // document.querySelector(".content p").style.display = "block";
+}
+
+joke();
